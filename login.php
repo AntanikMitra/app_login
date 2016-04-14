013dee6d55cbf02a1b95856f1963e909e0583b3e
antanikmitra <antanikmitra@hotmail.com>
2016-04-14 12:13:28 -0400

    first commit

---
+++ login.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<meta content="text/html; charset=utf-8" http-equiv="Content-Type">
+<title>Login</title>
+<link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+</head>
+
+<body>
+
+<form method="post" action="validate.php">
+<div>
+	<label for="username">Username:</label>
+	<input name="username" />
+</div>
+<div>
+	<label for="password">Password:</label>
+	<input name="password" type="password" />
+</div>
+<input type="submit" value="Login" />
+</form>
+
+</body>
+
+</html>
